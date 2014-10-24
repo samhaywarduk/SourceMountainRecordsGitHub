395555990919ad558a27a11efef38be6d168c748
Sam Hayward <samhaywarduk@gmail.com>
2014-10-24 23:50:11 +0100

    More Gallery and lightbox
    Tablet and desktop gallery settings added. Added a JQuery lightbox for
    the images when clicked.

--- gallery.php
+++ gallery.php
@@ -2,39 +2,40 @@
 <?php include('inc/header.php'); ?>
 	<h2>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle.</h2>
 
-	<section >
+	<section class="oFloAuto">
 
-		<ul class ="image-gallery">
-			<li>
-				<a href="http://lorempixel.com/640/480/nightlife/">
-					<img src="http://lorempixel.com/640/480/nightlife/"></img>
-					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
-				</a>
-			</li><li>
+		<ul class="image-gallery clear">
+			<li class="oFloAuto">
 				<a href="http://lorempixel.com/640/480/nightlife/">
 					<img src="http://lorempixel.com/640/480/nightlife/"></img>
 					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
 				</a>
 			</li>
-			<li>
+			<li class="oFloAuto">
 				<a href="http://lorempixel.com/640/480/nightlife/">
 					<img src="http://lorempixel.com/640/480/nightlife/"></img>
 					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
 				</a>
 			</li>
-			<li>
+			<li class="oFloAuto">
 				<a href="http://lorempixel.com/640/480/nightlife/">
 					<img src="http://lorempixel.com/640/480/nightlife/"></img>
 					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
 				</a>
 			</li>
-			<li>
+			<li class="oFloAuto">
 				<a href="http://lorempixel.com/640/480/nightlife/">
 					<img src="http://lorempixel.com/640/480/nightlife/"></img>
 					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
 				</a>
 			</li>
-			<li>
+			<li class="oFloAuto">
+				<a href="http://lorempixel.com/640/480/nightlife/">
+					<img src="http://lorempixel.com/640/480/nightlife/"></img>
+					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
+				</a>
+			</li>
+			<li class="oFloAuto">
 				<a href="http://lorempixel.com/640/480/nightlife/">
 					<img src="http://lorempixel.com/640/480/nightlife/"></img>
 					<p>Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle.</p>
--- inc/footer.php
+++ inc/footer.php
@@ -73,5 +73,39 @@
 		// 	}
 
 		// });
+
+		var $overlay = $('<div class="overlay"></div>');
+		var $image = $("<img>");
+		var $caption = $("<p></p>");
+
+		//Add image to overlay
+		$overlay.append($image);
+
+		//Add caption to overlay
+		$overlay.append($caption);
+
+		//Add overlay
+		$("body").append($overlay);
+
+		//Capture the click event on a link to an image
+		$(".image-gallery a").click(function(event){
+		  event.preventDefault();
+		  var imageLocation = $(this).attr("href");
+		  //Update overlay with the image linked in the link
+		  $image.attr("src", imageLocation);
+		  
+		  //Show the overlay.
+		  $overlay.show();
+		  
+		  //Get child's alt attribute and set caption
+		  var captionText = $(this).children("p").text();
+		  $caption.text(captionText);
+		});
+
+		//When overlay is clicked
+		$overlay.click(function(){
+		  //Hide the overlay
+		  $overlay.hide();
+		});	
 	</script>
 </html>
