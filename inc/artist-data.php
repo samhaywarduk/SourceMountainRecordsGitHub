e06b75c021826563368e98e85ae0793f9e0e1215
Sam Hayward <samhaywarduk@gmail.com>
2014-12-11 19:58:13 +0000

    Change header image and added more artists

--- inc/artist-data.php
+++ inc/artist-data.php
@@ -40,16 +40,44 @@ $artist_data["Rhain"] = array (
 								 '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/64603069&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
 								 '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/44624832&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
 );
-$artist_data["Randomer"] = array (
-	"name" => "Randomer",
-	"image" => "img/placeholder.jpg",
-	"artistsPageImage" => "img/placeholder-record.png",
-	"bio" => "Lorizzle ipsizzle RANDOMER dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle, gravida vizzle, shiz. Pellentesque own yo' gangster. Sed erizzle. Gangsta izzle dolizzle dapibizzle turpis tempizzle sure. Maurizzle pellentesque nibh gangster yippiyo. Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.
+$artist_data["SimianLeopard"] = array (
+	"name" => "Simian Leopard",
+	"image" => "img/artist7.png",
+	"artistsPageImage" => "img/artist7-record.png",
+	"bio" => "Lorizzle ipsizzle SIENNA CHORUS dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle, gravida vizzle, shiz. Pellentesque own yo' gangster. Sed erizzle. Gangsta izzle dolizzle dapibizzle turpis tempizzle sure. Maurizzle pellentesque nibh gangster yippiyo. Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.
 				Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.",
-	"video" => array('<iframe width="360" height="203" src="//www.youtube.com/embed/WSKyASxPChk" frameborder="0" allowfullscreen></iframe>',
-							'<iframe width="560" height="315" src="//www.youtube.com/embed/DMDFzvYK2t8" frameborder="0" allowfullscreen></iframe>'),
-	"soundCloud" => array('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/89352398&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
-								 '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/64603069&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>',
-								 '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/44624832&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
+	"links" => array("soundCloud" => "https://soundcloud.com/siennachorus",),				
+	"video" => array('<iframe width="360" height="203" src="//www.youtube.com/embed/puyDFO68iSs" frameborder="0" allowfullscreen></iframe>'),
+	"soundCloud" => array('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/170151976&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
+);
+$artist_data["RedSeaCreatures"] = array (
+	"name" => "Red Sea Creatures",
+	"image" => "img/artist6.png",
+	"artistsPageImage" => "img/artist6-record.png",
+	"bio" => "Lorizzle ipsizzle SIENNA CHORUS dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle, gravida vizzle, shiz. Pellentesque own yo' gangster. Sed erizzle. Gangsta izzle dolizzle dapibizzle turpis tempizzle sure. Maurizzle pellentesque nibh gangster yippiyo. Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.
+				Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.",
+	"links" => array("soundCloud" => "https://soundcloud.com/siennachorus",),				
+	"video" => array('<iframe width="360" height="203" src="//www.youtube.com/embed/puyDFO68iSs" frameborder="0" allowfullscreen></iframe>'),
+	"soundCloud" => array('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/170151976&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
+);
+$artist_data["GilPaulsen"] = array (
+	"name" => "Gil Paulsen",
+	"image" => "img/artist4.png",
+	"artistsPageImage" => "img/artist4-record.png",
+	"bio" => "Lorizzle ipsizzle SIENNA CHORUS dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle, gravida vizzle, shiz. Pellentesque own yo' gangster. Sed erizzle. Gangsta izzle dolizzle dapibizzle turpis tempizzle sure. Maurizzle pellentesque nibh gangster yippiyo. Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.
+				Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.",
+	"links" => array("soundCloud" => "https://soundcloud.com/siennachorus",),				
+	"video" => array('<iframe width="360" height="203" src="//www.youtube.com/embed/puyDFO68iSs" frameborder="0" allowfullscreen></iframe>'),
+	"soundCloud" => array('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/170151976&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
+);
+$artist_data["TonyMoe"] = array (
+	"name" => "Tony Moe",
+	"image" => "img/artist5.png",
+	"artistsPageImage" => "img/artist5-record.png",
+	"bio" => "Lorizzle ipsizzle SIENNA CHORUS dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle, gravida vizzle, shiz. Pellentesque own yo' gangster. Sed erizzle. Gangsta izzle dolizzle dapibizzle turpis tempizzle sure. Maurizzle pellentesque nibh gangster yippiyo. Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.
+				Vestibulum in tortor. Pellentesque break yo neck, yall rhoncizzle nisi. In hizzle habitasse platea dictumst. Phat dapibizzle. Curabitur tellus urna, pretizzle brizzle, mattizzle ac, eleifend fo shizzle mah nizzle fo rizzle, mah home g-dizzle, nunc. Cool suscipizzle. Integer semper velizzle sizzle bling bling.",
+	"links" => array("soundCloud" => "https://soundcloud.com/siennachorus",),				
+	"video" => array('<iframe width="360" height="203" src="//www.youtube.com/embed/puyDFO68iSs" frameborder="0" allowfullscreen></iframe>'),
+	"soundCloud" => array('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/170151976&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>')
 );
 ?>
--- inc/gallery-data.php
+++ inc/gallery-data.php
@@ -2,32 +2,32 @@
 
 $gallery_data = array();
 $gallery_data[101] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery6_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
 $gallery_data[102] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery2_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
 $gallery_data[103] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery3_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
 $gallery_data[104] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery4_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
 $gallery_data[105] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery5_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
 $gallery_data[106] = array(
-	'source' => 'http://lorempixel.com/640/480/nightlife/',
+	'source' => 'img/gallery1_640.jpg',
 	'caption' => "Lorizzle ipsizzle dolor check out this amizzle, consectetuer adipiscing elit. That's the shizzle that's the shizzle velit, for sure volutpizzle, suscipit ma nizzle."
 
 );
